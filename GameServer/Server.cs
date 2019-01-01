e59206939e2642c00ca093bfdfb0aa3aa4051804
liron.li <liron.li@outlook.com>
2019-01-01 14:22:45 +0800

    添加 log4net

--- GameServer/Server.cs
+++ GameServer/Server.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using log4net;
+using log4net.Config;
 using Photon.SocketServer;
+
+using LogManager = ExitGames.Logging.LogManager;
 
 namespace GameServer
 {
     public class Server : ApplicationBase
     {
+        private static readonly ILogger log = LogManager.GetCurrentClassLogger();
+
         protected override PeerBase CreatePeer(InitRequest initRequest)
         {
             return new Peer(initRequest.Protocol, initRequest.PhotonPeer);
@@ -16,12 +25,17 @@ namespace GameServer
 
         protected override void Setup()
         {
+            LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+            GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(this.ApplicationRootPath, "log");
+            GlobalContext.Properties["LogFileName"] = "MS" + this.ApplicationName;
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(this.BinaryPath, "log4net.config")));
 
+            log.Debug("GameServer is runing");
         }
 
         protected override void TearDown()
         {
-
+            log.Debug("GameServer is stop");
         }
     }
 }
